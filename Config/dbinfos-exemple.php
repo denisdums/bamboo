4c24bc7a6bf13551d70edcbf6f4abcab942b2d55
denisdums <dumontdd59@gmail.com>
2020-10-09 14:50:07 +0200

    bamboo initialize

---
+++ Config/dbinfos-exemple.php
@@ -0,0 +1,6 @@
+<?php
+define("HOST", "enter here your host");
+define("USERNAME", "enter here your username");
+define("DBNAME", "enter here your database name");
+define("PASSWORD", "enter here your database passowrd");
+
